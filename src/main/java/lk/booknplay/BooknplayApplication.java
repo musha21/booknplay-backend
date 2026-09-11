0e8e0645c885a2db3b7fcbe495bc6dfe5c748bc0
musha21 <mrmusha2021@gmail.com>
2026-09-11 12:25:52 +0530

    first commit

---
+++ src/main/java/lk/booknplay/BooknplayApplication.java
@@ -0,0 +1,13 @@
+package lk.booknplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooknplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooknplayApplication.class, args);
+	}
+
+}
